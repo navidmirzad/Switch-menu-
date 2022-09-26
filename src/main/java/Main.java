4ba24decc415a710033cdb83ea5846565c7930de
navidmirzad <navi0221@stud.kea.dk>
2022-09-26 14:44:51 +0200

    Oprettet scanner + velkomst

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Velkommen til Adventure af Navid...");
+        Scanner input = new Scanner(System.in);
+
+       String menu = input.nextLine();
+
+
+    }
+}
---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,7 @@
+public class UserInterface {
+    public void start() {
+    }
+
+    
+
+}
